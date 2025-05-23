019fb0d5cb6cfcc796e1cac2b530bc28462f7aec
Codinghoulz <piyushkr0309@gmail.com>
2025-05-23 11:38:12 +0530

    Implemented Logistic Regressionfrom scratch and loaded diabetes dataset from Kaggle

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Replace this with your actual path to diabetes.csv
+
+
+# Read the dataset
+df = pd.read_csv(r'C:\Users\piyus\DiabetesPrediction\data\diabetes.csv')
+
+# Show first few rows
+print(df.head())
+
